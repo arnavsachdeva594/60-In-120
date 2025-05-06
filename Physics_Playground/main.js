c8f7a88fc344c315499797706a064a213ae56fd5
Arnav Sachdeva <arnav.sachdeva594@gmail.com>
2025-05-06 10:44:21 +0530

    Changed Project

--- Physics_Playground/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const page = document.body.dataset.page;
-    if (page === 'projectile') projectileSimulator();
-    if (page === 'pendulum') pendulumSimulator();
-    if (page === 'wave') waveSimulator();
-    if (page === 'electric') electricSimulator();
-    if (page === 'magnetic') magneticSimulator();
-  });
-  
